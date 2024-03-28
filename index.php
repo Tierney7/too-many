e22824ba68d5bcbefa641bb337d829e2f6fbc436
Matthew Tierney <matt@rankfuse.com>
2024-03-28 16:03:27 -0500

    fixed bug

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
 /*
-Plugin Name: Less is More
+Plugin Name: Less is More 
 Author: Matthew Tierney 
 */
 
 add_action('admin_notices', 'less_is_more_message');
 
 function less_is_more_message() {
-    $plugins = get_plugins();
-    $active_plugins_count = count($plugins);
+    $active_plugins = get_option('active_plugins');
+    $num_active_plugins = count($active_plugins);
     
     
-    if ($active_plugins_count > 5) {
-        echo '<div class="notice notice-info"><p>Less is more... Please delete unneeded plugins.</p></div>';
+    if ($num_active_plugins > 5) {
+        echo '<div class="notice notice-warning is-dismissible">';
+        echo '<p>Less is more... Please delete unneeded plugins.</p></div>';
+        echo '</div>'
     }
 }
+
+add_action('admin_notices', 'less_is_more_message');
