63f49d687a5850bb6ab76df05035213e99bfd582
Matthew Tierney <matt@rankfuse.com>
2024-03-28 15:50:08 -0500

    plugin count 5

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-echo 
+<?php
+/*
+Plugin Name: Less is More
+Author: Matthew Tierney 
+*/
+
+add_action('admin_notices', 'less_is_more_message');
+
+function less_is_more_message() {
+    $plugins = get_plugins();
+    $active_plugins_count = count($plugins);
+    
+    
+    if ($active_plugins_count > 5) {
+        echo '<div class="notice notice-info"><p>Less is more... Please delete unneeded plugins.</p></div>';
+    }
+}
