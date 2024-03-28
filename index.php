4dc2735c865a4f17f17d66d7bd0a2ddcf28ead16
Matthew Tierney <matt@rankfuse.com>
2024-03-28 16:18:19 -0500

    new

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 /*
 Plugin Name: Less is More 
+Description: Alerts users by displaying a message in the dashboard if too many plugins are installed
+Version: 1.0
 Author: Matthew Tierney 
 */
 
